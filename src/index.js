1c6f3459c7511522f3d0c1e466b57e8075afc7e2
jaugs <johnaugsburger@gmail.com>
2022-09-10 13:43:27 -0700

    added plugins/search by zip or city

--- src/index.js
+++ src/index.js
@@ -1,33 +1,56 @@
 import './style.css';
 import sun from './sun.svg';
-
-
-    const element = document.createElement('div');
-    element.innerHTML = 'hello';
-   // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = sun;
- 
-   element.appendChild(myIcon);
-  document.body.appendChild(element);
+import printMe from './print.js';
 
 
 const container = document.querySelector('.container');
-const header  = document.querySelector('.heading');
+const header = document.querySelector('.header');
+   const myIcon = new Image();
+   myIcon.src = sun;
+container.appendChild(myIcon);
 
-async function getWeatherData() {
-   // let weatherCall = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-   let weatherResponse = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7100239e64c481b759f7889a357558f4', {mode: 'cors'});
-   let weatherData = await weatherResponse.json();
-   let lattitute = weatherData[0].lat;
-   let longitude = weatherData[0].lon;
-    let unit = 'imperial'
+
+const dropdownButton = document.querySelector('.dropbtn');
+dropdownButton.onclick = () => {showSelections();}
   
-   let weatherCall = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lattitute}&lon=${longitude}&appid=7100239e64c481b759f7889a357558f4&units=${unit}`, {mode: 'cors'});
-   let weather = await weatherCall.json();
-   console.log(weather)
-   //console.log(weatherData[0].lat)
+   function showSelections() {
+      document.getElementById("drop").classList.toggle("show");
+ }
+ 
+ // Close the dropdown menu if the user clicks outside of it
+ window.onclick = function(event) {
+   if (!event.target.matches('.dropbtn')) {
+     var dropdowns = document.getElementsByClassName("dropdown-content");
+     var i;
+     for (i = 0; i < dropdowns.length; i++) {
+       var openDropdown = dropdowns[i];
+       if (openDropdown.classList.contains('show')) {
+         openDropdown.classList.remove('show');
+       }}}}
 
+const zipCode = document.getElementById('zipcode');
+const city = document.getElementById('city');
 
+zipCode.onclick = () => searchZip();
+city.onclick = () => searchCity();
+
+function searchZip () {
+   console.log('dddd')
+   dropdownButton.innerText = "Zip Code:"
 }
-getWeatherData();
+// async function getWeatherData() {
+//    // let weatherCall = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//    let weatherResponse = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7100239e64c481b759f7889a357558f4', {mode: 'cors'});
+//    let weatherData = await weatherResponse.json();
+//    let lattitute = weatherData[0].lat;
+//    let longitude = weatherData[0].lon;
+//     let unit = 'imperial'
+  
+//    let weatherCall = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lattitute}&lon=${longitude}&appid=7100239e64c481b759f7889a357558f4&units=${unit}`, {mode: 'cors'});
+//    let weather = await weatherCall.json();
+//    console.log(weather)
+//    //console.log(weatherData[0].lat)
+
+
+// }
+// getWeatherData();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+      template: path.join(__dirname, 'src', 'index.html')
+    }),
+  ],
+  mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
